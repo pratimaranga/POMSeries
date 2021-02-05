0739716914b2908cc09b0956f01ec899a512f33c
Pratima <pratima.ranga@gmail.com>
2021-02-04 20:49:20 -0500

    contacUs page added

---
+++ src/main/java/com/qa/democart/pages/ContactUsPage.java
@@ -0,0 +1,13 @@
+package com.qa.democart.pages;
+
+public class ContactUsPage {
+	public ContactUsPage() {
+		System.out.println("ContactUsPage -- default const...");
+	}
+	
+	public void getTitle() {
+		System.out.println("ContactUsPage - title");
+		
+	}
+
+}
