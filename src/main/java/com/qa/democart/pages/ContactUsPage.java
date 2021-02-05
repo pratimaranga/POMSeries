9dcb91dc5ccd5738a2e2f8c0c33f0b38eca6b0d3
Pratima <pratima.ranga@gmail.com>
2021-02-04 23:05:47 -0500

    contactUs page updated

--- src/main/java/com/qa/democart/pages/ContactUsPage.java
+++ src/main/java/com/qa/democart/pages/ContactUsPage.java
@@ -1,13 +1,18 @@
 package com.qa.democart.pages;
 
+import org.openqa.selenium.By;
+
 public class ContactUsPage {
 	public ContactUsPage() {
 		System.out.println("ContactUsPage -- default const...");
 	}
 	
+	private By inputName = By.id("input-name");
+	private By emailId = By.id("input-email");
+	
 	public void getTitle() {
 		System.out.println("ContactUsPage - title");
-		
+		System.out.println("page edited");
 	}
 
 }
